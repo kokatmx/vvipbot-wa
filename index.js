0425d8901b275b605a6969f5493e5b9632fa9070
kokatmx <68942869+kokatmx@users.noreply.github.com>
2021-12-09 22:45:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:KEi\n' // Nama kamu
+            + 'ORG:Gignz;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6283115822576:+62 831-1582-2576\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
